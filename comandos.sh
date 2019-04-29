b1c063b8f4198e606064d440ab99129a68376584
yovanychb <38058822+yovanychb@users.noreply.github.com>
2019-04-29 16:30:18 -0600

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -6,3 +6,9 @@ nano rama.ldif
 ldapadd -D "cn=admin,dc=quesitos,dc=com" -W -h 127.0.0.1 -x -f rama.ldif
 nano cambiarPsw.ldif 
 ldapadd -Y EXTERNAL -H ldapi:/// -f cambiarPsw.ldif
+
+ldapsearch -x -LLL -b "dc=quesitos,dc=com" -D "cn=lector,dc=quesitos,dc=com" -W -h 127.0.0.1 "uid=jorge.gonzales"
+ldapsearch -x -LLL -b "dc=quesitos,dc=com" -D "cn=admin,dc=quesitos,dc=com" -W -h 127.0.0.1 "cn=lector"
+#uso de and
+ldapsearch -x -LLL -b "dc=quesitos,dc=com" -D "cn=admin,dc=quesitos,dc=com" -W -h 127.0.0.1 "(&(objectClass=organizationalRole)(cn=lector))"
+#alternativas a changetype

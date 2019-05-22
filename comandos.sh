e6237297d6aeaa926c6f9ca0b693eed5146ad2f9
yovanychb <38058822+yovanychb@users.noreply.github.com>
2019-05-22 16:58:04 -0600

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -4,6 +4,9 @@ apt install slapd
 apt install ldap-utils
 nano rama.ldif
 ldapadd -D "cn=admin,dc=quesitos,dc=com" -W -h 127.0.0.1 -x -f rama.ldif
+
+slappasswd -h {MD5} //cambiar pssw
+
 nano cambiarPsw.ldif 
 ldapadd -Y EXTERNAL -H ldapi:/// -f cambiarPsw.ldif
 
